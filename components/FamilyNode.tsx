8b5cea44ac70ec8f551865df9b82a833d62f49b1
clivetc <clivetchikwape@gmail.com>
2025-05-04 21:00:13 +0100

    quick fix

--- components/FamilyNode.tsx
+++ components/FamilyNode.tsx
@@ -1,38 +1,54 @@
 import React from "react";
-import { Box, Text, VStack, HStack } from "@chakra-ui/react";
+import { Box, Text, VStack, Flex } from "@chakra-ui/react";
 import { IFamily } from "~/interfaces/family.interface";
 import { formatDate } from "~/constants/date";
 
-const FamilyNode = ({ member }: { member: IFamily }) => {
+const FamilyNode = ({
+	member,
+	parentName,
+}: {
+	member: IFamily;
+	parentName?: string;
+}) => {
 	return (
-		<VStack align="center" position="relative">
-			<Box
-				p={4}
-				borderRadius="lg"
-				boxShadow="md"
-				borderWidth={2}
-				borderColor="gray.300"
-				bg="white"
-				minW="150px"
-				textAlign="center"
-			>
-				<Text fontWeight="bold" fontSize="md">
-					{member.name}
+		<VStack
+			p={4}
+			borderRadius="lg"
+			boxShadow="sm" // Softer shadow for the base
+			bg="blue.50"
+			textAlign="center"
+			border="1px solid #E2E8F0"
+			_hover={{
+				boxShadow: "md", // Lighter shadow on hover
+				transform: "scale(1.02)", // Subtle scaling on hover
+				cursor: "pointer",
+			}}
+			transition="all 0.3s ease"
+		>
+			<Text fontWeight="bold" color="blue.800" fontSize="xl">
+				{member.name}
+			</Text>
+			{member.birthDate && (
+				<Text fontSize="sm" color="gray.600">
+					Born: {formatDate(member.birthDate)}
 				</Text>
-				{member.birthDate && (
-					<Text fontSize="sm" color="gray.500">
-						Born: {formatDate(member.birthDate)}
-					</Text>
-				)}
-			</Box>
+			)}
+			{parentName && (
+				<Text fontSize="sm" color="gray.500">
+					Parent: {parentName}
+				</Text>
+			)}
 
-			{/* Render children horizontally */}
-			{member.children && member.children.length > 0 && (
-				<HStack align="flex-start" justify="center" wrap="wrap">
+			{member.children && (
+				<Flex mt={4} justify="center" wrap="wrap" direction="row" gap={8}>
 					{member.children.map((child) => (
-						<FamilyNode key={child.id} member={child} />
+						<FamilyNode
+							key={child.id}
+							member={child}
+							parentName={member.name}
+						/>
 					))}
-				</HStack>
+				</Flex>
 			)}
 		</VStack>
 	);
